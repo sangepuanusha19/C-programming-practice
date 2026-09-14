47fae2efa803acce2bb9290c4d62afbe74ea3d07
Sangepu Anusha <sangepuanusha1911@gmail.com>
2026-09-14 20:34:45 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main()
+{
+    int a,b,multiplication;
+    printf("enter the values of a,b");
+    scanf("%d%d",&a,&b);
+    multiplication=a*b;
+    printf("multiplication of a,b:%d",multiplication);
+    return 0;
+}
