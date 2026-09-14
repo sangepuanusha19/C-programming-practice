111b996f2e790a2aa359cb2013aa1feea9e4a16c
Sangepu Anusha <sangepuanusha1911@gmail.com>
2026-09-14 20:30:06 +0530

    Add files via upload

---
+++ subtraction.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main()
+{
+    int a, b,sub ;
+    printf("Enter the values of a , b:");
+    scanf("%d%d",&a,&b);
+    sub=a-b;
+    printf("subtraction of the two value is: %d",sub );
+    return 0;
+}
