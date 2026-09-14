a088140217c6c5c47c096012f5ff81f3e5621a22
Sangepu Anusha <sangepuanusha1911@gmail.com>
2026-09-14 20:30:33 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main()
+{
+    float a,b,division;
+    printf("enter the values of a,b");
+    scanf("%f%f",&a,&b);
+    division=a/b;
+    printf("division of a,b:%f",division);
+    return 0;
+}
