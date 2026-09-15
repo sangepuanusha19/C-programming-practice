8b8e85420db49660eff7545f0b3d14b9f7bf1c61
Sangepu Anusha <sangepuanusha1911@gmail.com>
2026-09-15 21:57:26 +0530

    Add files via upload

---
+++ area3.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main()
+{
+    float area,side;
+    printf("enter the sides of the square");
+    scanf("%f",&side);
+    area=side*side;
+    printf("the area of the square:%f",area);
+    return 0;
+}
