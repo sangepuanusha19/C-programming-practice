1d52fa813ae55b74375c1cd8ccd3705056001c7d
Sangepu Anusha <sangepuanusha1911@gmail.com>
2026-09-15 21:57:09 +0530

    Add files via upload

---
+++ area2.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main()
+{
+   float area,length,height; 
+   printf("enter the value of length,height");
+   scanf("%f%f",&length,&height);
+   area of the triangle=0.5*length*height;
+   printf("area of the triangle:%f",area);
+   return 0;
+}
