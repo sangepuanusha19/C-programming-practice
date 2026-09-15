9fbb17aa88437a3109b098b28d025906559461b7
Sangepu Anusha <sangepuanusha1911@gmail.com>
2026-09-15 21:57:44 +0530

    Add files via upload

---
+++ area4.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main()
+{
+    float area,length,breadth;
+    printf("enter the values of length and breadt");
+    scanf("%f%f",&length,&breadth);
+    area=length*breadth;
+    printf("area of the rectangle:%f",area);
+    return 0;
+}
